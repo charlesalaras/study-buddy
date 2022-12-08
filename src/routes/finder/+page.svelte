fb222de0ac721c82a4a6ba57aa988e739d862cf3
Charles Alaras <charles.alaras@gmail.com>
2022-12-08 14:14:59 -0800

    Add finding capability

--- src/routes/finder/+page.svelte
+++ src/routes/finder/+page.svelte
@@ -1,25 +1,80 @@
-<script>
+<script lang="ts">
+    import { supabase } from "$lib/supabaseClient";
+    let active: boolean = true;
+    let buddies = [];
 
+    let school: string | null = null;
+    let major: string | null = null;
+    let classes: string | null = null;
+
+    async function runQuery() {
+        if(!school && !major && !classes) {
+            alert("Please enter a query!");
+            return;
+        }
+        
+        const query = {
+            ...(school ? {school} : {}),
+            ...(major ? {major} : {}),
+        };
+        try {
+            let { data, error } = await supabase.from('profiles').select()
+                .match(query)
+                .contains('courses', [classes]);
+            console.log(data);
+            buddies = data;
+        if (error) throw error
+        } catch (error) {
+            if (error instanceof Error) {
+                alert(error.message)
+            }
+        } finally {
+            console.log("end of function!");
+            active = false;
+            console.log(active);
+        }
+    }
 </script>
 
 <div>
-    <form>
+    {#if active}
+    <form on:submit|preventDefault="{runQuery}">
         <div class="form-block">
         <label for="school">University</label>
-        <input class="field" type="text" placeholder="University" id="school"/>
+        <input class="field" type="text" placeholder="University" id="school" bind:value="{school}"/>
         </div>
         <div class="form-block">
         <label for="school">Major</label>
-        <input class="field" type="text" placeholder="Major" id="major"/>
+        <input class="field" type="text" placeholder="Major" id="major" bind:value="{major}"/>
         </div>
         <div class="form-block">
         <label for="school">Courses</label>
-        <input class="field" type="text" placeholder="Courses" id="courses"/>
+        <input class="field" type="text" placeholder="Courses" id="courses" bind:value="{classes}"/>
         </div>
         <div class="form-submit">
             <button class="submit"><span class="material-symbols-outlined">search</span>FIND MY STUDY BUDDY</button>
         </div>
     </form>
+    {:else}
+        <button class="return" on:click="{() => {active = !active}}">
+            <span class="material-symbols-outlined">arrow_back_ios</span>Return
+        </button>
+        {#each buddies as buddy}
+            <div class="card">
+                <div class="profile-pic">
+
+                </div>
+                <div class="card-details">
+                    <div style="font-size: 1.5em; font-weight: 700">{buddy.username}</div>
+                    <div>{buddy.school}</div>
+                    <div>{buddy.major}</div>
+                    <button class="return">
+                        <span style="margin-right: 5px" class="material-symbols-outlined">person_add</span> Study with Buddy
+                    </button>
+                </div>
+            </div>
+        {/each}
+    {/if}
 </div>
 
 <style>
@@ -29,6 +84,34 @@
         'wght' 700,
         'GRAD' 0,
         'opsz' 40
+    }
+    .return {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: #381E72;
+        background-color: #C4AFF4;
+        border: none;
+        font-weight: 700;
+        padding: 10px 20px 10px 20px;
+        border-radius: 40px;
+    }
+    .card {
+        padding: 10px;
+        margin-top: 10px;
+        margin-bottom: 10px;
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        border: 2px solid #49454F;
+        border-radius: 12px;
+    }
+    .card-details {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        gap: 5px;
     }
     .form-submit {
         display: flex;
